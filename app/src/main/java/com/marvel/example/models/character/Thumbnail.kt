04185ef2e986613e4943e29e2ed83f7ba430a48e
Kurt Renzo Acosta <kurt.r.acosta@gmail.com>
2019-04-21 23:51:19 +0800

    Adds JsonClass annotation on Thumbnail

--- app/src/main/java/com/marvel/example/models/character/Thumbnail.kt
+++ app/src/main/java/com/marvel/example/models/character/Thumbnail.kt
@@ -1,7 +1,9 @@
 package com.marvel.example.models.character
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class Thumbnail(
 	@Json(name="path")
 	val path: String,
